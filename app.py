20e8842f88a19ccb520066eb3577e584308f38d0
Luis Muñoz <luis152288@gmail.com>
2025-03-31 05:23:01 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    return jsonify({"message": "Hello, World!"})
+
+@app.route('/api/data', methods=['GET'])
+def data():
+    return jsonify({"data": [1, 2, 3, 4, 5]})
+
+if __name__ == '__main__':
+    app.run(debug=True)
